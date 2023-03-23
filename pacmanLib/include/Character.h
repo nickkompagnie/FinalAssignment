2c1e59dc4cc9cd0b25201a2bec0e69cb180c2901
nickkompagnie <125453522+nickkompagnie@users.noreply.github.com>
2023-03-23 11:54:21 +0100

    character

---
+++ pacmanLib/include/Character.h
@@ -0,0 +1,31 @@
+//==============================================================
+// Filename : Character.h
+// Authors : Quinten Boeve and Nick Kompagnie
+// Group : 17
+// License : N.A. or open source license like LGPL
+// Description : Base class for all character
+//==============================================================
+
+#ifndef CHARACTER
+#define CHARACTER
+
+#include "GameObjectStruct.h"
+
+
+/// Base class of collectibles
+class Character : public GameObjectStruct
+{
+    
+
+
+public:
+
+void die() {
+
+
+}
+
+
+};
+
+#endif
